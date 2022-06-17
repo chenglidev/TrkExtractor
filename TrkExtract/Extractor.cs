ef3831f152a2e98de50dbb4b02e5f8f5b6bab7ab
Cheng <93148706+chenglidev@users.noreply.github.com>
2022-06-18 00:12:53 +0700

    Add project files.

---
+++ TrkExtract/Extractor.cs
@@ -0,0 +1,53 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TrkExtract
+{
+    public class Extractor
+    {
+        public void Extract(string xmlFilePath, string csvFilePath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFilePath);
+
+            var elTrkpts = doc.GetElementsByTagName("trkpt");
+
+            var csvConfig = new CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture);
+            using (StringWriter writer = new StringWriter())
+            {
+                writer.WriteLine("Lat,Lon,Depth");
+                using (CsvWriter csvWriter = new CsvWriter(writer, csvConfig))
+                {
+                    foreach (XmlNode elTrkpt in elTrkpts)
+                    {
+                        var lat = elTrkpt.Attributes["lat"].Value;
+                        var lon = elTrkpt.Attributes["lon"].Value;
+
+                        var exts = elTrkpt.FirstChild;
+                        var depth = "";
+                        foreach (XmlNode hNode in exts.ChildNodes)
+                        {
+                            if (hNode.Name != "h:depth") continue;
+                            depth = hNode.InnerText;
+                            break;
+                        }
+
+
+                        csvWriter.WriteField(lat);
+                        csvWriter.WriteField(lon);
+                        csvWriter.WriteField(depth);
+                        csvWriter.NextRecord();
+                    }
+                }
+                File.WriteAllText(csvFilePath, writer.ToString());
+            }
+        }
+    }
+}
---
+++ TrkExtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrkExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Argument missing. Please input xml file path.");
+                return;
+            }
+            try {
+                Extractor extractor = new Extractor();
+                extractor.Extract(args[0], "Output.csv");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}
